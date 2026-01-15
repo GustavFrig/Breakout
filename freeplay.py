40a4de36ff2898b7a7ebdb7d216ee53a98cd006d
GustavFrig <gustav.frigaard@gmail.com>
2026-01-15 14:08:27 +0100

    Klosser og ball til rekt

--- freeplay.py
+++ freeplay.py
@@ -7,27 +7,57 @@ pg.init()
 vindu = pg.display.set_mode((VINDU_BREDDE, VINDU_HOYDE))
 pg.display.set_caption("Freeplay")
 
-class Sirkel:
-    def __init__(self, x, y, radius, farge, vy):
+class Ball:
+    def __init__(self, x, y, bredde, hoyde, farge, vx, vy):
         self.x = x
         self.y = y
-        self.radius = radius
+        self.rect = pg.Rect(x, y, bredde, hoyde)
         self.farge = farge
+        self.vx = vx
         self.vy = vy
 
     def oppdater(self):
-        self.y += self.vy
-        if self.y > VINDU_HOYDE - self.radius: 
-            self.vy *= -1  
-            self.y = VINDU_HOYDE - self.radius  
-        elif self.y <= 0 + self.radius:  
-            self.vy *= -1  
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+        if self.rect.bottom >= VINDU_HOYDE or self.rect.top <= 0:
+            self.vy *= -1
+
+        if self.rect.right >= VINDU_BREDDE or self.rect.left <= 0:
+            self.vx *= -1
 
     def tegn(self, vindu):
-        pg.draw.circle(vindu, self.farge, (self.x, self.y), self.radius)
+        pg.draw.rect(vindu, self.farge, self.rect)
+
+class Rekt:
+    def __init__(self, x, y, bredde, hoyde, farge):
+        self.rect = pg.Rect(x, y, bredde, hoyde)
+        self.farge = farge
+        self.aktiv = True  # om klossen fortsatt finnes
+
+    def tegn(self, vindu):
+        if self.aktiv:
+            pg.draw.rect(vindu, self.farge, self.rect)
 
 
-sirkel = Sirkel(x=400, y=300, radius=30, farge=(255, 0, 0), vy=5)
+ball = Ball(
+    x=400,
+    y=300,
+    bredde=15,
+    hoyde=15,
+    farge=(255, 0, 0),
+    vx=1,
+    vy=1
+)
+for rad in range(ANTALL_RADER):
+    for kol in range(ANTALL_KOLONNER):
+        x = MARGIN + kol * (KLOSS_BREDDE + MARGIN)
+        y = START_Y + rad * (KLOSS_HOYDE + MARGIN)
+        klosser.append(
+            Rekt(x, y, KLOSS_BREDDE, KLOSS_HOYDE, (0, 150, 255))
+        )
+
+
 
 running = True
 while running:
@@ -39,8 +69,16 @@ while running:
 
     vindu.fill(WHITE)
 
-    sirkel.oppdater()
-    sirkel.tegn(vindu)
+    ball.oppdater()
+    for kloss in klosser:
+        if kloss.aktiv and ball.rect.colliderect(kloss.rect):
+            kloss.aktiv = False
+            ball.vy *= -1
+            break
+
+    ball.tegn(vindu)
+    for kloss in klosser:
+        kloss.tegn(vindu)
 
   
     pg.display.update()
