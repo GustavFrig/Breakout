5172727d2ade8c46eb8644e9aed928d2d3b34845
FredrikWangen <fredrik.wangen07@gmail.com>
2026-01-17 10:58:16 +0100

    lagde en meny

--- freeplay.py
+++ freeplay.py
@@ -2,77 +2,75 @@ import pygame as pg
 from constants import *
 from objekt import *
 
-pg.init()
+def freeplay():
+    pg.init()
 
-FPS = FPS
-clock = pg.time.Clock()
-
-
-vindu = pg.display.set_mode((VINDU_BREDDE, VINDU_HOYDE))
-pg.display.set_caption("Freeplay")
-
-
-ball = Ball(
-    x=400,
-    y=300,
-    bredde=15,
-    hoyde=15,
-    farge=(255, 0, 0),
-    vx=5,
-    vy=5
-)
-platform = Platform(farge=BLUE, x=500, y=500)
-
-for rad in range(ANTALL_RADER):
-    for kol in range(ANTALL_KOLONNER):
-        x = MARGIN + kol * (KLOSS_BREDDE + MARGIN)
-        y = START_Y + rad * (KLOSS_HOYDE + MARGIN)
-        klosser.append(
-            Rekt(x, y, KLOSS_BREDDE, KLOSS_HOYDE, (0, 150, 255))
-        )
+    FPS = 60
+    clock = pg.time.Clock()
+    vindu = pg.display.set_mode((VINDU_BREDDE, VINDU_HOYDE))
+    pg.display.set_caption("Freeplay")
 
 
+    ball = Ball(
+        x=400,
+        y=300,
+        bredde=15,
+        hoyde=15,
+        farge=(255, 0, 0),
+        vx=5,
+        vy=5
+    )
+    platform = Platform(farge=BLUE, x=500, y=500)
 
-running = True
-while running:
-    for event in pg.event.get():  
-        if event.type == pg.QUIT:  
-            running = False
-        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:  
-            running = False
-        elif ball.y==1:
-            running=False
+    for rad in range(ANTALL_RADER):
+        for kol in range(ANTALL_KOLONNER):
+            x = MARGIN + kol * (KLOSS_BREDDE + MARGIN)
+            y = START_Y + rad * (KLOSS_HOYDE + MARGIN)
+            klosser.append(
+                Rekt(x, y, KLOSS_BREDDE, KLOSS_HOYDE, (0, 150, 255))
+            )
 
-    vindu.fill(WHITE)
 
-    ball.oppdater()
-    for kloss in klosser:
-        if kloss.aktiv and ball.rect.colliderect(kloss.rect):
-            kloss.aktiv = False
-            ball.vy *= -1
-            break
 
-    ball.tegn(vindu)
-    for kloss in klosser:
-        kloss.tegn(vindu)
+    running = True
+    while running:
+        for event in pg.event.get():  
+            if event.type == pg.QUIT:  
+                running = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:  
+                running = False
+            elif ball.rect.top>VINDU_HOYDE:
+                running=False
+
+        vindu.fill(WHITE)
+
+        ball.oppdater()
     
-    if ball.rect.colliderect(platform.rect):
-       if ball.rect.centerx < platform.rect.left or ball.rect.centerx > platform.rect.right:
-            ball.vx *= -1
-       elif ball.rect.centery < platform.rect.centery:
-           ball.vy *= -1
-           ball.rect.bottom = platform.rect.top
-       else:
-           ball.vy *= -1
+        for kloss in klosser:
+            if kloss.aktiv and ball.rect.colliderect(kloss.rect):
+                kloss.aktiv = False
+                ball.vy *= -1
+                break
+    
+        if ball.rect.colliderect(platform.rect):
+            if ball.rect.centerx < platform.rect.left or ball.rect.centerx > platform.rect.right:
+                ball.vx *= -1
+            elif ball.rect.centery < platform.rect.centery:
+                ball.vy *= -1
+                ball.rect.bottom = platform.rect.top
+            else:
+                ball.vy *= -1
     
     
+        ball.tegn(vindu)
+        for kloss in klosser:
+            kloss.tegn(vindu)
+  
+        platform.oppdater()
+        platform.tegn(vindu)
 
   
-    platform.oppdater()
-    platform.tegn(vindu)
-
-  
-    pg.display.flip()
-    clock.tick(FPS)
+        pg.display.flip()
+        clock.tick(FPS)
 
 pg.quit()
---
+++ meny.py
@@ -0,0 +1,44 @@
+import pygame as pg
+from constants import *
+from freeplay import freeplay
+
+pg.init()
+
+vindu = pg.display.set_mode((VINDU_BREDDE, VINDU_HOYDE))
+pg.display.set_caption("Meny")
+clock = pg.time.Clock()
+
+
+font = pg.font.SysFont(None, 40)
+
+freeplay_rektangel = pg.Rect(200, 120, 200, 50)
+level_rektangel = pg.Rect(200, 200, 200, 50)
+
+running = True
+while running:
+    pg.display.set_caption("Meny")
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if freeplay_rektangel.collidepoint(event.pos):
+                freeplay()
+            if level_rektangel.collidepoint(event.pos):
+                pass
+
+    
+    vindu.fill((GREEN))
+
+    pg.draw.rect(vindu, (70, 70, 200), freeplay_rektangel)
+    pg.draw.rect(vindu, (70, 200, 70), level_rektangel)
+
+
+    vindu.blit(font.render("Freeplay", True, WHITE), (250, 135))
+    vindu.blit(font.render("Mode 2", True, WHITE), (250, 215))
+
+
+    pg.display.flip()
+    clock.tick(60)
+
+pg.quit()
